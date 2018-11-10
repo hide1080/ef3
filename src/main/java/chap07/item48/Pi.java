5d8f84ff2f79d41471243759e53db93c4606cbd1
hide1 <hide1080@gmail.com>
2018-11-10 18:21:02 +0900

    微修正

--- src/main/java/chap07/item48/Pi.java
+++ src/main/java/chap07/item48/Pi.java
@@ -6,14 +6,12 @@ import java.util.stream.LongStream;
 public class Pi {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        long c = pi((long) Math.pow(10, 7));
-        System.out.println(System.currentTimeMillis() - start);
-        System.out.println(c);
+        long p = pi((long) Math.pow(10, 7));
+        report(start, p);
 
         start = System.currentTimeMillis();
-        c = pi((long) Math.pow(10, 7));
-        System.out.println(System.currentTimeMillis() - start);
-        System.out.println(c);
+        p = pi((long) Math.pow(10, 7));
+        report(start, p);
     }
 
     static long pi (long n) {
@@ -30,4 +28,9 @@ public class Pi {
                 .filter(i -> i.isProbablePrime(50))
                 .count();
     }
+
+    static void report(long s, long p ) {
+        System.out.println(String.format("response time: %s, result: %s", System.currentTimeMillis() - s, p));
+    }
+
 }
